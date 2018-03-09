0915e390bb38eac22fb3bb763ac0853ace3c44d7
Tyler <dru1d@dru1d.ninja>
2018-03-09 13:32:25 -0500

    added parity check

--- wiegand-dec2hex.py
+++ wiegand-dec2hex.py
@@ -2,14 +2,21 @@
 ###dru1d@dru1d.ninja###
 ###Taken from ccdesignworks.com/wiegand_calc.html###
 
+def bitCount(int_type):
+    count = 0
+    while(int_type):
+        int_type &= int_type - 1
+        count += 1
+    return(count)
+
 while True:
 ###Variables and stuff###
 	FACILITY_CODE = int(raw_input("Insert FC: "))
 	BADGE_ID	  = int(raw_input("Insert BadgeID: "))
-	# FAC_PAR_MASK  = 0b10000000000000000000000000
-	# FACILITY_MASK = 0b01111111100000000000000000
-	# CARD_MASK     = 0b00000000011111111111111110
-	# CARD_PAR_MASK = 1;
+	FAC_PAR_MASK  = 0b10000000000000000000000000
+	FACILITY_MASK = 0b01111111100000000000000000
+	CARD_MASK     = 0b00000000011111111111111110
+	CARD_PAR_MASK = 1;
 	FCH = format(FACILITY_CODE, 'x')
 	BIH = format(BADGE_ID, 'x')
 	FCH_PADDED = "{:02x}".format(int(FCH, 16))
@@ -26,7 +33,6 @@ while True:
 		bit = bin(int(i))
 		if bit != 0:
 			UPPER_PARITY ^= 1
-	print("UPPER PARITY: %s" % str(UPPER_PARITY))
 
 ####Lower Parity Bit (ODD)
 	LOWER_PARITY = 1
@@ -34,14 +40,34 @@ while True:
 		bit = bin(int(i))
 		if bit != 0:
 			LOWER_PARITY ^= 1
-	print("LOWER PARITY: %s" % str(LOWER_PARITY))
 
 ###Final Badge Values###
-	BINARY_BADGE = str(LOWER_PARITY) + CARD_BINARY + str(UPPER_PARITY)
-	HEX_BADGE = hex(int(BINARY_BADGE,2))
+	BINARY_BADGE = int(str(LOWER_PARITY) + CARD_BINARY + str(UPPER_PARITY), 2)
+	HEX_BADGE = hex(BINARY_BADGE)
+
+###Parity Check###
+	FACILITY = (BINARY_BADGE & FACILITY_MASK) >> 17
+	CARD = (BINARY_BADGE & CARD_MASK) >> 1
+	
+	FACILITY_PAR = (BINARY_BADGE & FAC_PAR_MASK) >> 25
+	CARD_PAR = (BINARY_BADGE & CARD_PAR_MASK)
+####Even Parity Check
+	FAC_PAR_OK = (bitCount(FACILITY) + FACILITY_PAR) % 2 == 0
+####Odd Parity Check
+	CARD_PAR_OK = (bitCount(CARD) + CARD_PAR) % 2 ==1
+
+
+
 
 ###Print Statements###	
-	print("FC (HEX): 0x%s" % str(FCH_PADDED))
-	print("Badge ID(HEX): 0x%s" % str((BIH_PADDED)))
-	print("Binary Card Data: %s" % BINARY_BADGE)
-	print("Hex Card Data: %s" % HEX_BADGE)
+	if FAC_PAR_OK and CARD_PAR_OK:
+		print("Both parity bits OK. Success!" + "\n")
+		print("FC (HEX): 0x%s" % str(FCH_PADDED))
+		print("Badge ID(HEX): 0x%s" % str((BIH_PADDED)))
+		print("Binary Card Data: %s" % str(BINARY_BADGE))
+		print("Hex Card Data: %s" % HEX_BADGE)
+	else:
+		if not FAC_PAR_OK:
+			print("Facility parity check failed.")
+		if not CARD_PAR_OK:
+			print("Card parity check failed.")
